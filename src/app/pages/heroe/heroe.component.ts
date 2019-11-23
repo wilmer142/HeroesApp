3f94990fb4ad1a40750ccdbb7f119b5786b81973
wilmer142 <wilmercollege@gmail.com>
2019-11-22 22:13:16 -0500

    Implementing getAll and get on service

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -5,17 +5,31 @@ import { HeroesService } from 'src/app/services/heroes.service';
 
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent {
+export class HeroeComponent implements OnInit {
 
   heroe = new HeroeModel(); 
 
-  constructor(private heroeService: HeroesService) { }
+  constructor(private heroeService: HeroesService,
+              private route:ActivatedRoute) { }
+
+  ngOnInit(){
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id !== 'nuevo') {
+      this.heroeService.getHeroe(id)
+        .subscribe( (resp: HeroeModel) => {
+          this.heroe = resp;
+          this.heroe.id = id;
+        });
+    }
+  }
 
   guardar(form:NgForm){
 
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -31,4 +31,31 @@ export class HeroesService {
 
     return this.http.put(`${this.url}/heroes/${heroe.id}.json`, heroeTemp);
   }
+
+  getHeroe(id:string){
+    return this.http.get(`${this.url}/heroes/${id}.json`);
+  }
+
+  getHeroes(){
+    return this.http.get(`${this.url}/heroes.json`)
+      .pipe(
+        map( resp => this.crearArregloDeHeroes(resp))
+      );
+  }
+
+  private crearArregloDeHeroes(heroesObj: object) {
+    
+    const heroes:HeroeModel[] = [];
+    
+    if (heroesObj !== null) { 
+      Object.keys(heroesObj).forEach( key => {
+        const heroe:HeroeModel = heroesObj[key];
+        heroe.id = key;
+  
+        heroes.push(heroe);
+      });
+    }
+
+    return heroes;
+  }
 }
