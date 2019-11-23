d97687238e47572dfcf8ee29fefcb83f4d75a4b9
wilmer142 <wilmercollege@gmail.com>
2019-11-22 21:38:41 -0500

    Implementing post and put methods on heroes service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesComponent } from './pages/heroes/heroes.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { FormsModule } from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,23 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵSWITCH_IVY_ENABLED__POST_R3__ } from '@angular/core';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { NgForm } from '@angular/forms';
+import { HeroesService } from 'src/app/services/heroes.service';
+
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
   heroe = new HeroeModel(); 
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private heroeService: HeroesService) { }
 
   guardar(form:NgForm){
-    console.log(form);
-    console.log(this.heroe);
+
+    if (form.invalid) return;
+
+    Swal.fire({
+      title: 'Expere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if (this.heroe.id) {
+      peticion = this.heroeService.actualizarHeroe(this.heroe);
+    } else{
+      peticion = this.heroeService.crearHeroe(this.heroe);
+    }
+
+    peticion.subscribe(resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        icon: 'success'
+      })
+    });
   }
 }
