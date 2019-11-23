4f7d57fde6734fec965d614c28f43d1b522b6488
wilmer142 <wilmercollege@gmail.com>
2019-11-22 22:38:00 -0500

    Implementing loading and no data alerts

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -11,12 +11,17 @@ import Swal from 'sweetalert2';
 export class HeroesComponent implements OnInit {
 
   heroes:HeroeModel[] = [];
+  cargando:boolean = false;
 
   constructor(private heroesService:HeroesService) {}
 
   ngOnInit() {
+    this.cargando = true;
     this.heroesService.getHeroes()
-      .subscribe( resp => this.heroes = resp);
+      .subscribe( resp => {
+        this.heroes = resp;
+        this.cargando = false;
+      });
   }
 
   borrarHeroe(heroe:HeroeModel, i:number){
